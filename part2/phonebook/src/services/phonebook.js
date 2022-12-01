a703309a20f733beee8b5dac8c0b66accb7a691c
Mikhail Sukhenko <tymah@yahoo.com>
2022-12-01 14:44:16 -0500

    moving forward

---
+++ part2/phonebook/src/services/phonebook.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const base_url = 'http://localhost:3001/persons'
+
+const getAll = () => {
+    const request = axios.get(base_url)
+    return request.then(response => response.data)
+}
+
+
+const create = person => {
+    const request = axios.post(base_url, person)
+    return request.then(response => response.data)
+} 
+
+const deletePerson = (id) => {
+    const request = axios.delete(`${base_url}/${id}`)
+    return request.then(response => response)
+}
+
+const update = person => {
+    const request = axios.put(`${base_url}/${person.id}`, person)
+    return request.then(response => response.data)
+}
+
+const phonebookService = {getAll, create, deletePerson, update}
+
+export default  phonebookService
